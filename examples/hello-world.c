9548768ae94a434a335ac142bcd28f3fb20a0617
edilson258 <dev.258.edilson@gmail.com>
2024-06-02 07:16:35 +0200

    feat: make it a header only library

---
+++ examples/hello-world.c
@@ -0,0 +1,61 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MPSC_IMPLEMENTATION
+#include "../include/mpsc.h"
+
+#define ONE_SEC_IN_MICRO_SEC 1000000
+
+char *prepare_str_msg(char *str) {
+  unsigned long str_len = strlen(str);
+  char *message = malloc(sizeof(char) * (str_len + 1));
+  strncpy(message, str, str_len);
+  return message;
+}
+
+void *producer_hello(void *arg) {
+  Channel *chan = (Channel *)arg;
+  // sleep for 1 sec to simulate some work
+  usleep(ONE_SEC_IN_MICRO_SEC);
+  // send the message through channel
+  send(chan, prepare_str_msg("Hello"));
+  return NULL;
+}
+
+void *producer_world(void *arg) {
+  Channel *chan = (Channel *)arg;
+  // sleep for 3 sec to simulate some work
+  usleep(ONE_SEC_IN_MICRO_SEC * 3);
+  // send the message through channel
+  send(chan, prepare_str_msg("World"));
+  return NULL;
+}
+
+void *receiver(void *arg) {
+  Channel *chan = (Channel *)arg;
+  for (int i = 0; i < 2; ++i) {
+    char *msg = (char *)recv(chan);
+    if (msg) {
+      printf("Received: %s\n", msg);
+      free(msg);
+    }
+  }
+  return NULL;
+}
+
+int main(void) {
+  Channel *chan = channel_init();
+
+  pthread_t p1, p2, receiver_id;
+  pthread_create(&p1, NULL, producer_hello, chan);
+  pthread_create(&p2, NULL, producer_world, chan);
+  pthread_create(&receiver_id, NULL, receiver, chan);
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
+  pthread_join(receiver_id, NULL);
+
+  return 0;
+}
--- include/mpsc.h
+++ include/mpsc.h
@@ -3,7 +3,18 @@
 
 #include <pthread.h>
 
-#include "queue.h"
+/// This structure wraps the message in a linked list node to be stored in the
+/// messages queue
+typedef struct Node {
+  void *message;
+  struct Node *next;
+} Node;
+
+/// This struct holds the head of the queue of messages and some other metadata
+/// about the queue
+typedef struct {
+  Node *head;
+} MessagesQueue;
 
 /// This structure represents a channel through which multiple
 /// threads can pass and receive messages
@@ -14,16 +25,113 @@ typedef struct {
 } Channel;
 
 Channel *channel_init(void);
-
-/// Sends a message through the channel and wakes at least one thread 
+/// Sends a message through the channel and wakes at least one thread
 /// sleeping or waiting for some message
 int send(Channel *chan, void *message);
-
-
 /// This functions receive a message from the channel
 /// will take first avalible message in the Queue
 /// if the channel is empty will block the current thread until some message be
 /// send
 void *recv(Channel *chan);
+MessagesQueue *queue_init();
+Node *node_init(void *message);
+void *dequeue(MessagesQueue *q);
+int enqueue(MessagesQueue *q, void *message);
+int is_queue_empty(MessagesQueue *q);
 
-#endif
+#ifdef MPSC_IMPLEMENTATION
+
+#include <errno.h>
+#include <stdio.h>
+
+Channel *channel_init(void) {
+  Channel *chan = malloc(sizeof(Channel));
+  chan->queue = queue_init();
+  pthread_mutex_init(&chan->mutex, NULL);
+  pthread_cond_init(&chan->cond, NULL);
+  return chan;
+}
+
+int send(Channel *chan, void *message) {
+  pthread_mutex_lock(&chan->mutex);
+  if (enqueue(chan->queue, message)) {
+    pthread_mutex_unlock(&chan->mutex);
+    return -1;
+  }
+  // tells at least one thread waiting about the new message
+  pthread_cond_signal(&chan->cond);
+  pthread_mutex_unlock(&chan->mutex);
+  return 0;
+}
+
+void *recv(Channel *chan) {
+  pthread_mutex_lock(&chan->mutex);
+  // block until get some message
+  while (is_queue_empty(chan->queue)) {
+    pthread_cond_wait(&chan->cond, &chan->mutex);
+  }
+  void *message = dequeue(chan->queue);
+  pthread_mutex_unlock(&chan->mutex);
+  return message;
+}
+
+int is_queue_empty(MessagesQueue *q) { return q->head == NULL; }
+
+MessagesQueue *queue_init() {
+  MessagesQueue *queue = malloc(sizeof(MessagesQueue));
+  if (queue == NULL) {
+    fprintf(stderr, "[Error]: Couldn't allocate space for message node: %s\n",
+            strerror(errno));
+    return NULL;
+  }
+  queue->head = NULL;
+  return queue;
+}
+
+Node *node_init(void *message) {
+  Node *node = malloc(sizeof(Node));
+  if (node == NULL) {
+    fprintf(stderr, "[Error]: Couldn't allocate space for a node: %s\n",
+            strerror(errno));
+    return NULL;
+  }
+  node->message = message;
+  node->next = NULL;
+  return node;
+}
+
+/// This function will push a new message at the end of the queue, once the
+/// queue is a linked list the complexity might be O(n)
+int enqueue(MessagesQueue *q, void *message) {
+  Node *node = node_init(message);
+  if (node == NULL) {
+    return -1;
+  }
+
+  if (!q->head) {
+    q->head = node;
+  } else {
+    /*
+     * O(n)
+     */
+    Node *tmp = q->head;
+    while (tmp->next)
+      tmp = tmp->next;
+    tmp->next = node;
+  }
+  return 0;
+}
+
+void *dequeue(MessagesQueue *q) {
+  if (is_queue_empty(q)) {
+    return NULL;
+  }
+  Node *node = q->head;
+  q->head = node->next;
+  void *message = node->message;
+  free(node);
+  return message;
+}
+
+#endif // MPSC_IMPLEMENTATION
+#endif // MPSC_H
