51152c099b4d6c435d6c2273a25555961630ca42
= <dev.258.edilson@gmail.com>
2024-06-23 12:18:22 +0200

    feat: support multi-thread

--- src/Stack.c
+++ src/Stack.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,32 +9,47 @@ Stack_t Stack_new(size_t max_stack) {
   stack->head = NULL;
   stack->size = 0;
   stack->max_stack = max_stack;
+  pthread_mutex_init(&stack->mutx, NULL);
   return stack;
 }
 
-Boolean Stack_is_empty(Stack_t stack) { return stack->head == NULL; }
-Boolean Stack_is_full(Stack_t stack) { return stack->size >= stack->max_stack; }
+Boolean Stack_is_empty(Stack_t stack) {
+  pthread_mutex_lock(&stack->mutx);
+  Boolean is_empty = stack->head == NULL;
+  pthread_mutex_unlock(&stack->mutx);
+  return is_empty;
+}
 
-void Stack_push(Stack_t stack, Any_t data) {
+Boolean Stack_is_full(Stack_t stack) {
+  pthread_mutex_lock(&stack->mutx);
+  Boolean is_full = stack->size >= stack->max_stack;
+  pthread_mutex_unlock(&stack->mutx);
+  return is_full;
+}
+
+int Stack_push(Stack_t stack, Any_t data) {
   if (stack->max_stack <= -1 && Stack_is_full(stack)) {
-    fprintf(stderr, "[Error]: StackOverFlow\n");
-    exit(1);
+    return -1;
   }
   Node_t node = Node_new(data);
+  pthread_mutex_lock(&stack->mutx);
   node->next = stack->head;
   stack->head = node;
   stack->size++;
+  pthread_mutex_unlock(&stack->mutx);
+  return 0;
 }
 
 Any_t Stack_pop(Stack_t stack) {
   if (Stack_is_empty(stack)) {
-    fprintf(stderr, "[Error]: StackUnderFlow\n");
-    exit(1);
+    return NULL;
   }
+  pthread_mutex_lock(&stack->mutx);
   Node_t tmp = stack->head;
   stack->head = tmp->next;
   stack->size--;
   Any_t data = tmp->data;
   free(tmp);
+  pthread_mutex_unlock(&stack->mutx);
   return data;
 }
